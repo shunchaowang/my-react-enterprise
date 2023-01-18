72441bdb2c372f583d6059af430b77760c6e1f7b
Shunchao Wang <shunchao.wang@gmail.com>
2023-01-17 19:21:10 -0500

    wip

---
+++ postcss.config.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  plugins: [
+    require('postcss-flexbugs-fixes'),
+    require('stylelint')({
+      configFile: 'stylelint.config.cjs',
+    }),
+    require('postcss-import'),
+    require('postcss-extend'),
+    require('postcss-mixins'),
+    // Comment out postcss-nested if you're using tailwindcss/nesting require('postcss-nested'),
+    // require('tailwindcss/nesting'),
+    // require('tailwindcss')('tailwind.config.js'),
+    require('postcss-preset-env', {
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false,
+        'nesting-rules': false,
+      },
+    }),
+    require('autoprefixer')(),
+    require('postcss-reporter'),
+  ],
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,7 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: { extend: {} },
+  // Add only if you want to use the @tailwindcss/forms package
+  plugins: [require('@tailwindcss/forms')],
+}
